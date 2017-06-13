ea04267e25945c1c84ab8db1f85df0633ce10482
刘祺 <qil@jumei.com>
2017-06-13 10:43:54 +0800

    update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,23 @@ const rename = require('gulp-rename');
 const stylefmt = require('gulp-stylefmt');
 gulp.task('default', () => {
 	return gulp.src('../autoprefixer/test/cases/*.css')
-	.pipe(stylefmt())
-	.pipe(rename(function (path) {
-		if (/\.out$/.test(path.basename)) {
-			path.basename = path.basename.replace(/\.out$/, '');
-		} else {
-			path.basename += '.out';
-		}
-	}))
-	.pipe(gulp.dest('./test/fixtures'));
+		.pipe(stylefmt())
+		.pipe(rename(function (path) {
+			if (/\.out$/.test(path.basename)) {
+				path.basename = path.basename.replace(/\.out$/, '');
+			} else {
+				path.basename += '.out';
+			}
+		}))
+		.pipe(gulp.dest('./test/fixtures'));
 });
 gulp.task('test', () => {
 	return gulp.src('./test/fixtures.bak/**/*.css')
-	.pipe(stylefmt())
-	.pipe(rename(function (path) {
-		if (/\Wout$/.test(path.basename)) {
-			path.basename = path.basename.replace(/\Wout$/, '.out');
-		}
-	}))
-	.pipe(gulp.dest('./test/fixtures'));
+		.pipe(stylefmt())
+		.pipe(rename(function (path) {
+			if (/\Wout$/.test(path.basename)) {
+				path.basename = path.basename.replace(/\Wout$/, '.out');
+			}
+		}))
+		.pipe(gulp.dest('./test/fixtures'));
 });
